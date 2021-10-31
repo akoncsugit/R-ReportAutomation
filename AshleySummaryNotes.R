f870afe7926db32692fa475d49553416924cccb7
akoncsugit <aastadle@ncsu.edu>
2021-10-31 12:08:41 -0400

    Updated gitignore to exclude notes

--- AshleySummaryNotes.R
+++ AshleySummaryNotes.R
@@ -1,9 +1,1 @@
-downSized <- subset_data %>% select(n_tokens_title, n_tokens_content,
-                                    num_imgs:average_token_length,
-                                    kw_avg_avg, is_weekend,
-                                    global_subjectivity,
-                                    global_sentiment_polarity,
-                                    global_rate_negative_words,
-                                    avg_negative_polarity,
-                                    abs_title_subjectivity:shares)
-corrplot(cor(downSized))
+ignore this
